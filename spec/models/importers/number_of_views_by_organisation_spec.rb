2123bda6a030912308d5bc54b63a5b17a4953b43
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2017-02-20 18:05:34 +0000

    Set an attribute for batch size

--- spec/models/importers/number_of_views_by_organisation_spec.rb
+++ spec/models/importers/number_of_views_by_organisation_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Importers::NumberOfViewsByOrganisation do
     let!(:organisation) { create(:organisation, slug: 'the-slug') }
     let!(:content_item_first) { create(:content_item, base_path: 'the-link/first', organisations: [organisation]) }
     let!(:content_item_second) { create(:content_item, base_path: 'the-link/second', organisations: [organisation]) }
+    let!(:content_item_third) { create(:content_item, base_path: 'the-link/third', organisations: [organisation]) }
 
     it "updates the number of views for all content items" do
       allow_any_instance_of(GoogleAnalyticsService).to receive(:page_views).and_return(
@@ -19,6 +20,8 @@ RSpec.describe Importers::NumberOfViewsByOrganisation do
           },
         ]
       )
+
+      subject.batch_size = 1
       subject.run('the-slug')
 
       content_item_one = ContentItem.find_by(base_path: 'the-link/first')
@@ -28,10 +31,20 @@ RSpec.describe Importers::NumberOfViewsByOrganisation do
       expect(content_item_two.unique_page_views).to eq(2)
     end
 
-    it "perform the requests in batches" do
-      expect_any_instance_of(GoogleAnalyticsService).to receive(:page_views).twice.and_return([])
+    context "performs the request to google api in batches" do
+      it "makes three requests when the batch size is one" do
+        expect_any_instance_of(GoogleAnalyticsService).to receive(:page_views).exactly(3).times.and_return([])
 
-      subject.run('the-slug')
+        subject.run('the-slug')
+      end
+
+      it "makes two requests when the batch size is two" do
+        expect_any_instance_of(GoogleAnalyticsService).to receive(:page_views).twice.and_return([])
+
+        subject.batch_size = 2
+
+        subject.run('the-slug')
+      end
     end
   end
 end

4d1ae32979b5e2da0a6ba0bc86d89ea1b2b8acbe
Lee Briggs <lee.briggs@digital.cabinet-office.gov.uk>
2017-02-24 12:40:02 +0000

    Removed location of metrics

--- app/services/content_items_service.rb
+++ app/services/content_items_service.rb
@@ -8,44 +8,11 @@ class ContentItemsService
     Clients::SearchAPI.find_each(query: query, fields: fields) do |response|
       base_path = response.fetch(:link)
       content_item = Clients::ContentStore.find(base_path, attribute_names)
-
-      puts build_metrics(content_item)
-      puts content_item.fetch(:base_path)
-      # TODO: merge enhanced metrics into content item - depends on DB changes 
-      # yield metrics.reduce(content_item, :merge) if content_item
-      cleanup_attributes(content_item)
-
       yield content_item if content_item
     end
   end
 
 private
-
-  def build_metrics(content_item)
-    metrics = []
-    all_metrics.each do |klass|
-       metrics.push klass.new(content_item).build
-    end
-    metrics
-  end
-
-  def all_metrics
-    [
-      Metrics::NumberOfPdfsMetric
-    ]
-  end
-
-  def cleanup_attributes(content_item)
-    cleanable_attribute_names.each do |k|
-      content_item.delete k
-    end
-  end
-
-  def cleanable_attribute_names
-    [
-      :details
-    ]
-  end
 
   def attribute_names
     @names ||= %i(content_id description title public_updated_at document_type base_path details)
